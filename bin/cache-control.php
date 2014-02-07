c457c847fb8933240159919f90108ad97be7cd71
Sebastian Krebs <krebs.seb@gmail.com>
2014-02-07 14:02:28 +0100

    Fix autoload path for dep-installations

--- bin/cache-control.php
+++ bin/cache-control.php
@@ -1,7 +1,7 @@
 <?php
 namespace Crunch\CacheControl;
 
-require __DIR__ . '/../vendor/autoload.php';
+(@include_once __DIR__ . '/../vendor/autoload.php') || @include_once __DIR__ . '/../../../autoload.php';
 
 $console = new Console\Application;
 
