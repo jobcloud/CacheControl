1c88a4af5c940bc13f587a6bfee8bfb1fe27443a
Sebastian Krebs <krebs.seb@gmail.com>
2014-04-27 02:46:47 +0200

    Removed superfluous Application-class

--- bin/cache-control.php
+++ bin/cache-control.php
@@ -1,8 +1,14 @@
 <?php
 namespace Crunch\CacheControl;
 
+use Crunch\CacheControl\Console\ClearCommand;
+use Crunch\CacheControl\Console\StatusCommand;
+use Symfony\Component\Console\Application;
+
 (@include_once __DIR__ . '/../vendor/autoload.php') || @include_once __DIR__ . '/../../../autoload.php';
 
-$console = new Console\Application;
+$console = new Application;
+
+$console->addCommands(array(new ClearCommand(), new StatusCommand()));
 
 $console->run();
